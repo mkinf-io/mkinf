cc7115a78d7ff4b38d7a90205054883bf71fcf7f
Gionatan Cernusco <gionatan99@gmail.com>
2025-01-29 19:51:33 +0100

    feat: adds actions support

--- src/mkinf/hub/pull.py
+++ src/mkinf/hub/pull.py
@@ -8,7 +8,7 @@ load_dotenv()
 
 
 def pull(repos: list[str]) -> list[BaseTool]:
-    if os.getenv('MKINF_API_KEY') is None:
+    if not os.getenv('MKINF_API_KEY'):
         raise ValueError("Missing MKINF_API_KEY")
 
     tools = []
@@ -20,17 +20,18 @@ def pull(repos: list[str]) -> list[BaseTool]:
     if res.status_code != 200:
         raise Exception("Can't load tools")
     for repo in res.json()["data"]:
-        latest_release = repo.get("releases", [])[0]
-
-        params = ", ".join(latest_release["input_schema"].keys())
-        method_body = f"""
+        # TODO: Get correct version
+        release = repo.get("releases", [])[0]
+        for action in release["actions"]:
+            params = ", ".join(action["input_schema"].keys())
+            method_body = f"""
 def _run(self, {params}):
     import requests
     try:
         # Build the body dynamically from parameters
-        body = {{{", ".join(f'"{param}": {param}' for param in latest_release["input_schema"].keys())}}}
+        body = {{{", ".join(f'"{param}": {param}' for param in action["input_schema"].keys())}}}
         response = requests.post(
-            url=f"https://run.dev.mkinf.io/v0.1/{repo["owner"]}/{repo["name"]}/run",
+            url=f"https://run.dev.mkinf.io/v0.1/{repo["owner"]}/{repo["name"]}/{action["action"]}",
             headers={{"Authorization": f"Bearer {os.getenv('MKINF_API_KEY')}"}},
             json=body
         )
@@ -39,21 +40,21 @@ def _run(self, {params}):
         print(f"ERROR: {{e}}")
         return {{"error": str(e)}}
 """
-        class_attributes = {}
-        exec(method_body, {}, class_attributes)
-        DynamicClass = type(
-            f"{repo["owner"]}__{repo["name"]}",
-            (BaseTool,),
-            {
-                "__annotations__": {
-                    "name": str,
-                    "description": str,
+            class_attributes = {}
+            exec(method_body, {}, class_attributes)
+            DynamicClass = type(
+                f"{repo["owner"]}__{repo["name"]}__{action["action"]}",
+                (BaseTool,),
+                {
+                    "__annotations__": {
+                        "name": str,
+                        "description": str,
+                    },
+                    "name": Field(default=f"{repo["owner"]}__{repo["name"]}__{action["action"]}"),
+                    "description": Field(default=action["description"]),
+                    "_run": class_attributes["_run"],
                 },
-                "name": Field(default=latest_release["name"]),
-                "description": Field(default=latest_release["description"]),
-                "_run": class_attributes["_run"],
-            },
-        )
-        tools.append(DynamicClass())
+            )
+            tools.append(DynamicClass())
 
     return tools
